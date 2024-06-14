948098d1932fa5cffd11c250f3fffda166a8df29
9O2wnhwKDg0I <ilyasolonitsyn@mail.ru>
2024-06-14 21:47:26 +0300

    dummy services

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,32 @@
+from datetime import datetime
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import FileResponse, Response
+from PIL import Image
+import aiofiles
+import io
 import os
-
 import uvicorn
-from fastapi import FastAPI
+
+WORKDIR = '' # for docker
+TMP_DIR = f'{WORKDIR}tmp_files/'
 
 app = FastAPI()
+
+async def save_image(file_binary, filename= None):
+    if filename is None:
+        filename = f'tmp_{datetime.now()}_.jpg'
+
+    if not os.path.exists(TMP_DIR):
+        os.mkdir(TMP_DIR)
+
+    img_file_path = os.path.join(TMP_DIR, filename)
+
+    print(img_file_path)
+
+    async with aiofiles.open(img_file_path, 'wb') as out_file:
+        await out_file.write(file_binary) 
+
+    return img_file_path, file_binary
 
 
 @app.get("/")
@@ -11,20 +34,30 @@ def hello():
     return "Все робит"
 
 
-# @app.post("/signup/")
-# async def signup(request: Request):
-#     print("signup...")
-#     parsed_data = await request.json()
-#     dbresponse = db.signup(parsed_data)
-#     return JSONResponse(content=dbresponse, status_code=200)
+@app.post("/detect/")
+async def process_image(file: bytes = File(...)):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+    
+        img_file_path, binary_img_data = await save_image(file)
 
+        # classifier_probs, recognited_text, predict_img_path = pipeline.forward(img_file_path)
 
-# @app.post("/login/")
-# async def login(request: Request):
-#     parsed_data = await request.json()
-#     dbresponse = db.login(parsed_data)
-#     return JSONResponse(content=dbresponse, status_code=200)
+        # pipeline = Pipeline(WEIGHTS_DIR, TMP_DIR, classifier_weights_name = 'weights_71_0.94_0.93_0.93_0.93.pt')
+
+        # response = format_response_detect_client_prod(classifier_probs, recognited_text, predict_img_path)
+
+        response = {}
+
+        predict_img = Image.open(img_file_path)
+        bytes_image = io.BytesIO()
+
+        predict_img.save(bytes_image, format='PNG')
+        
+        return Response(content=bytes_image.getvalue(), headers=response, media_type="image/png")
 
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=os.getenv('BACKEND_PORT'))
+    # uvicorn.run(app, host="0.0.0.0", port=8228)
