ffa2f648d2d9b0d14b43209d12fb019b424effa6
9O2wnhwKDg0I <ilyasolonitsyn@mail.ru>
2024-06-16 04:06:00 +0300

    the bigest commit in this hack

--- backend/models/Detector.py
+++ backend/models/Detector.py
@@ -2,16 +2,73 @@ from ultralytics import YOLO
 import os
 from datetime import datetime
 from collections import Counter
+import numpy as np
+
+def iou(box1, box2):
+    """
+    Вычисляет коэффициент перекрытия (IoU) между двумя bounding boxes.
+    
+    :param box1: Координаты первого бокса [x1, y1, x2, y2]
+    :param box2: Координаты второго бокса [x1, y1, x2, y2]
+    :return: IoU
+    """
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+    
+    inter_area = max(0, x2 - x1) * max(0, y2 - y1)
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    
+    union_area = box1_area + box2_area - inter_area
+    
+    return inter_area / min(box1_area, box2_area)
+
+def non_maximum_suppression(confs, coords, iou_threshold=0.8):
+    """
+    Применяет Non-Maximum Suppression (NMS) для устранения перекрывающихся боксов.
+    
+    :param names: Список имен классов
+    :param confs: Список уверенности в обнаружении
+    :param coords: Список координат боксов [x1, y1, x2, y2]
+    :param iou_threshold: Порог IoU для определения перекрывающихся боксов
+    :return: Оставшиеся боксы после применения NMS
+    """
+    indices = np.argsort(confs)[::-1]
+    
+    keep_boxes = []
+    while len(indices) > 0:
+        current_index = indices[0]
+        current_box = coords[current_index]
+        
+        keep_boxes.append(current_index)
+        
+        iou_scores = np.array([iou(current_box, coords[i]) for i in indices[1:]])
+        
+        indices = indices[1:][iou_scores <= iou_threshold]
+        
+    return keep_boxes
 
 class Detector:
-    def __init__(self, path_to_weights: str, path_to_tmp: str, weights_name: str = 'detector_weights_v1.pt', confidence_level: float = 0.001):
+    def __init__(
+            self, 
+            path_to_weights: str, 
+            path_to_tmp: str, 
+            weights_name: str = 'detector_weights_v1.pt', 
+            confidence_level: float = 0.25, 
+            box_intersetor_flag: bool = False,
+            iou_treshold: float = 0.8
+        ):
         self.model = YOLO(os.path.join(path_to_weights, weights_name))
-        self.model.conf = confidence_level
+        self.confidence_level = confidence_level
         self.tmp_path = path_to_tmp
+        self.box_intersetor_flag = box_intersetor_flag
+        self.iou_treshold = iou_treshold
 
     def predict(self, paths_to_images: list):
         # print("Detection...")
-        outputs = self.model(paths_to_images, verbose=False)
+        outputs = self.model.predict(paths_to_images, verbose=False, conf = self.confidence_level)
 
         preds = []
 
@@ -32,5 +89,26 @@ class Detector:
                     "names_count": dict(Counter(names))
                 }
             )
-            
-        return preds
+
+        if not self.box_intersetor_flag:
+            return preds
+        
+        intersected_preds = []
+
+        for i, pred in enumerate(preds):
+            indices = non_maximum_suppression(pred["confs"], pred["coords"], iou_threshold=self.iou_treshold)
+
+            intersected_names = np.array(names)[indices]
+            intersected_preds.append(
+                {
+                    "predict_img_path": predict_img_path,
+                    "source_img_path": paths_to_images[i],
+                    "names": intersected_names,
+                    "confs": np.array(confs)[indices],
+                    "coords": output.boxes.xyxy.numpy()[indices],
+                    "xywhn": output.boxes.xywhn.numpy()[indices],
+                    "names_count": dict(Counter(intersected_names))
+                }
+            )
+
+        return intersected_preds
--- backend/models/predict.py
+++ backend/models/predict.py
@@ -3,7 +3,20 @@ import os
 from tqdm import tqdm
 import pandas as pd
 
-def format_output(predicts):
+def load_annotations(txt_file):
+    annotations = []
+    with open(txt_file, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            class_id = int(parts[0])
+            rel_x = float(parts[1])
+            rel_y = float(parts[2])
+            width = float(parts[3])
+            height = float(parts[4])
+            annotations.append((class_id, rel_x, rel_y, width, height))
+    return annotations
+
+def format_output(predicts, dataset_path='', for_public=False):
     formated_predicts = {
             'filename' : [],
             'class_id' : [],
@@ -32,6 +45,18 @@ def format_output(predicts):
             formated_predicts['rel_y'].append(parsed_coords[i][1])
             formated_predicts['width'].append(parsed_coords[i][2])
             formated_predicts['height'].append(parsed_coords[i][3])
+
+    if for_public:
+        for filename in os.listdir(dataset_path):
+            if filename.endswith('.txt') and not filename.startswith('5 '):
+                annotations = load_annotations(os.path.join(dataset_path, filename))
+                for annot in annotations:
+                    formated_predicts['filename'].append(filename.split('.')[0] + '.jpg')
+                    formated_predicts['class_id'].append(annot[0])
+                    formated_predicts['rel_x'].append(annot[1])
+                    formated_predicts['rel_y'].append(annot[2])
+                    formated_predicts['width'].append(annot[3])
+                    formated_predicts['height'].append(annot[4])
             
     return formated_predicts
 
@@ -40,17 +65,26 @@ def main(
         path_to_tmp='./tmp_files', 
         weights_name='detector_weights_v2.pt',
         dataset_path='../dataset',
-        submission_file_path='submission.csv'
+        submission_file_path='submission.csv',
+        for_public = False
         ):
 
-    model = Detector(path_to_weights=weights_path, path_to_tmp=path_to_tmp, weights_name=weights_name)
+    model = Detector(path_to_weights=weights_path, path_to_tmp=path_to_tmp, weights_name=weights_name, confidence_level=0.1,box_intersetor_flag=True)
+    
+    image_paths = None
 
-    image_paths = [os.path.join(dataset_path, filename) for filename in os.listdir(dataset_path) if filename.endswith('.jpg')]
+    if not for_public:
+        image_paths = [os.path.join(dataset_path, filename) for filename in os.listdir(dataset_path) if filename.endswith('.jpg')]
+    else:
+        image_paths = [os.path.join(dataset_path, filename) for filename in os.listdir(dataset_path) if filename.endswith('.jpg') and filename.startswith('5 ')]
 
     predicts = []
     for img in tqdm(image_paths):
         predicts += model.predict([img])
+        
 
-    pd.DataFrame(format_output(predicts)).to_csv(submission_file_path, index=False, sep=';')
+    pd.DataFrame(format_output(predicts, dataset_path, for_public)).to_csv(submission_file_path, index=False, sep=';')
 
-main()
+main(submission_file_path='weight_augmented.csv', for_public=True, weights_name='weights_augmented.pt')
+main(submission_file_path='weight_augmented_last.csv', for_public=True, weights_name='weights_augmented_last.pt')
+main(submission_file_path='V2_TRUE.csv', for_public=True, weights_name='detector_weights_v2.pt')
